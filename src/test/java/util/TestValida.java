f6842abb6e163ab62e0bbb00700f67ea69f75b8c
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2022-05-31 11:17:12 -0300

    Aumentando a qualidade.

--- src/test/java/util/TestValida.java
+++ src/test/java/util/TestValida.java
@@ -11,40 +11,70 @@ import org.junit.jupiter.api.Test;
 public class TestValida {
 
     private Valida valida = null;
-
+    
+   /**
+     * Instância uma classe de validação.
+     */
     @BeforeAll
     public void inicializa() {
         valida = new Valida();
     }
 
-    @AfterAll
-    public void finaliza() {
-        valida = null;
-    }
-
+    /**
+     * Testa CPF válido.
+     */
     @Test
     public void testValidaCPFValido1() {
         assertTrue(valida.validaCPF("11111111111"));
     }
 
+    /**
+     * Testa CPF válido.
+     */
     @Test
     public void testValidaCPFValido2() {
         assertTrue(valida.validaCPF("84807125206"));
     }
 
+    /**
+     * Testa CPF válido.
+     */
     @Test
     public void testValidaCPFValido3() {
         assertTrue(valida.validaCPF("63883136395"));
     }
 
+    /**
+     * Testa CPF válido.
+     */
     @Test
     public void testValidaCPFValido4() {
         assertTrue(valida.validaCPF("31626333033"));
     }
 
+    /**
+     * Testa CPF com problema na conversão.
+     */
     @Test
     public void testValidaCPFInvalido() {
         //CPF com problema na conversão
         assertFalse(valida.validaCPF("0065XAB22050"));
+    }
+    
+    /**
+     * Testa CPF com problema na quantidade de caracteres.
+     */
+    @Test
+    public void testValidaCPFInvalidoCurto() {
+        //CPF com problema na conversão
+        assertFalse(valida.validaCPF("111111"));
+    }
+
+    /**
+     * Finaliza a classe de validação.
+     */
+    @AfterAll
+    public void finaliza() {
+        valida = null;
     }
 }
