2f89c0e1afc2e97a80e8bbaa99212ddc3c43cb87
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2022-05-31 09:06:57 -0300

    Documentação dos testes.

--- src/test/java/dao/TestDAOConsulta.java
+++ src/test/java/dao/TestDAOConsulta.java
@@ -16,11 +16,17 @@ public class TestDAOConsulta {
 
     Cliente cliente;
 
+     /**
+     * Instância um cliente para os testes.
+     */
     @BeforeEach
     public void inicializa() {
         cliente = new Cliente("131", "Cliente Existente", "11111111111");
     }
 
+    /**
+     * Testa um cliente existente no SQLite.
+     */
     @Test
     public void testConsulta1() {
         DAOFactory factory = DAOFactory.getDAOFactory(DAOFactory.SQLITE);
@@ -32,6 +38,9 @@ public class TestDAOConsulta {
         assertNotEquals(0, lista.size());
     }
 
+    /**
+     * Testa um cliente existente no Hashmap.
+     */
     @Test
     public void testConsulta2() {
         DAOFactory factory = DAOFactory.getDAOFactory(DAOFactory.HASHMAP);
@@ -44,6 +53,9 @@ public class TestDAOConsulta {
         assertNotEquals(0, lista.size());
     }
 
+    /**
+     * Testa um cliente existente no RAF.
+     */
     @Test
     public void testConsulta3() {
         DAOFactory factory = DAOFactory.getDAOFactory(DAOFactory.RAF);
@@ -56,6 +68,11 @@ public class TestDAOConsulta {
         assertNotEquals(0, lista.size());
     }
 
+    /**
+     * Exclui os clientes usados nos testes.
+     * 
+     * @throws java.lang.Exception
+     */
     @AfterEach
     public void Finaliza() throws Exception {
         DAOFactory factory = DAOFactory.getDAOFactory(DAOFactory.SQLITE);
